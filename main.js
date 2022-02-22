07a75587ffcc67c63aeed9900ccdd1a2b423c152
Karr122s <95999822+Karr122s@users.noreply.github.com>
2022-02-22 17:29:23 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var a;
+var Foryou = document.getElementById("Foryou");
+var Micon = document.getElementById("Micon");
+var image = document.getElementById("image");
+
+function music(){
+    if(Foryou.paused){
+        Foryou.play();
+    }
+    else{Foryou.pause();}
+
+}
+
+function show_hide(){
+    if(a==1){
+        document.getElementById("image").style.display="none";
+        return a=0;
+    }
+    else{
+        document.getElementById("image").style.display="block";
+        return a=1;
+
+    }
+    
+}
